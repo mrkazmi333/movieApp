65533592e19a6014eaf2db61df2e5ecc526a3809
mrkcancode <motasimkazmi333@gmail.com>
2020-08-05 13:56:22 +0530

    subscribed to store

--- src/index.js
+++ src/index.js
@@ -8,18 +8,18 @@ import movies from './reducers';
 
 const store = createStore(movies);
 console.log('Store', store);
-console.log('Before State', store.getState());
+// console.log('Before State', store.getState());
 
-store.dispatch({
-  type: 'ADD_MOVIES',
-  movies: [{name: 'superman'}]
-});
-console.log('After State', store.getState());
+// store.dispatch({
+//   type: 'ADD_MOVIES',
+//   movies: [{name: 'superman'}]
+// });
+// console.log('After State', store.getState());
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App store={store} />
   </React.StrictMode>,
   document.getElementById('root')
 );
