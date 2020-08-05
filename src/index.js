9bf6e00234b59374b825c63c662865bcbe3851b8
mrkcancode <motasimkazmi333@gmail.com>
2020-08-05 13:40:02 +0530

    created store and dispatched actions

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+
 import './index.css';
 import App from './components/App';
+import movies from './reducers';
+
+const store = createStore(movies);
+console.log('Store', store);
+console.log('Before State', store.getState());
+
+store.dispatch({
+  type: 'ADD_MOVIES',
+  movies: [{name: 'superman'}]
+});
+console.log('After State', store.getState());
+
 
 ReactDOM.render(
   <React.StrictMode>
