d1640135d9adb6b99dd807f4858e7d42cbdec6b2
mrkcancode <motasimkazmi333@gmail.com>
2020-08-05 14:31:00 +0530

    added action creators and types

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { data } from '../data';
 import Navbar from './Navbar';
 import MovieCard from './MovieCard';
+import { addMovies } from '../actions';
 
 
 class App extends React.Component {
@@ -14,10 +15,7 @@ class App extends React.Component {
     })
     //make api call
     //dispatch action
-    store.dispatch({
-      type: 'ADD_MOVIES',
-      movies: data
-    });
+    store.dispatch(addMovies(data));
 
     console.log('STATE', this.props.store.getState());
 
