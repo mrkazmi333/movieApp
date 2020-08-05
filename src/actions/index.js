25dd606cb0c96e53ad975b1d6ac19cb99802bfce
mrkcancode <motasimkazmi333@gmail.com>
2020-08-05 16:55:05 +0530

    add and removing favourite movie

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,6 +6,9 @@
 
 //action types
 export const ADD_MOVIES = 'ADD_MOVIES';
+export const ADD_TO_FAVOURITES = 'ADD_TO_FAVOURITES';
+export const REMOVE_FROM_FAVOURITES = 'REMOVE_FROM_FAVOURITES';
+export const SET_SHOW_FAVOURITES = 'SET_SHOW_FAVOURITES';
 
 //action creators
 export function addMovies(movies) {
@@ -13,4 +16,25 @@ export function addMovies(movies) {
         type: ADD_MOVIES,
         movies: movies
     }   
-  }
+}
+
+export function addFavourite(movie) {
+    return {
+        type: ADD_TO_FAVOURITES,
+        movie
+        }   
+}
+
+export function removeFavourite(movie) {
+    return {
+        type: REMOVE_FROM_FAVOURITES,
+        movie
+    }
+}
+
+export function setShowFavourites(val) {
+    return {
+        type: SET_SHOW_FAVOURITES,
+        val
+    }
+}
