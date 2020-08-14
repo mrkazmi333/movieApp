5fb147a66f489ddd508ed1fd1972aebaf1d01e18
mrkcancode <motasimkazmi333@gmail.com>
2020-08-14 21:30:52 +0530

    added middleware for action logger

--- src/index.js
+++ src/index.js
@@ -1,20 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 
 import "./index.css";
 import App from "./components/App";
 import rootReducer from "./reducers";
 
-const store = createStore(rootReducer);
-console.log("Store", store);
-// console.log('Before State', store.getState());
+// const logger = function ({ dispatch, getState }) {
+//   //redux will automatically pass dispatch and getState function is logger function. This is not store
+//   return function (next) {
+//     return function (action) {
+//       //middleware code
+//       console.log("ACTION_TYPE = ", action.type);
+//       next(action);
+//     };
+//   };
+// };
 
-// store.dispatch({
-//   type: 'ADD_MOVIES',
-//   movies: [{name: 'superman'}]
-// });
-// console.log('After State', store.getState());
+const logger = ({ dispatch, getState }) => (next) => (action) => {
+  //logger code
+  console.log("ACTION_TYPE = ", action.type);
+  next(action);
+};
+
+const store = createStore(rootReducer, applyMiddleware(logger));
+console.log("Store", store);
 
 ReactDOM.render(
   // <React.StrictMode>
