1e614b618bd025082f4eb94014c3d4cb9bd29c50
mrkcancode <motasimkazmi333@gmail.com>
2020-08-14 17:17:16 +0530

    added combine reducers

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
 
-import './index.css';
-import App from './components/App';
-import movies from './reducers';
+import "./index.css";
+import App from "./components/App";
+import rootReducer from "./reducers";
 
-const store = createStore(movies);
-console.log('Store', store);
+const store = createStore(rootReducer);
+console.log("Store", store);
 // console.log('Before State', store.getState());
 
 // store.dispatch({
@@ -16,11 +16,9 @@ console.log('Store', store);
 // });
 // console.log('After State', store.getState());
 
-
 ReactDOM.render(
   // <React.StrictMode>
-    <App store={store} />,
+  <App store={store} />,
   // </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,48 +1,73 @@
-import { ADD_MOVIES, ADD_TO_FAVOURITES, REMOVE_FROM_FAVOURITES, SET_SHOW_FAVOURITES } from '../actions';
-
+import {
+  ADD_MOVIES,
+  ADD_TO_FAVOURITES,
+  REMOVE_FROM_FAVOURITES,
+  SET_SHOW_FAVOURITES,
+} from "../actions";
+import { combineReducers } from "redux";
 
 const initialMoviesState = {
-    list: [],
-    favourites: [],
-    showFavourites: false
+  list: [],
+  favourites: [],
+  showFavourites: false,
+};
+export function movies(state = initialMoviesState, action) {
+  // if(action.type === ADD_MOVIES) {
+  //     return {
+  //         ...state,
+  //         list: action.movies
+  //     }
+  // }
+  // return state;
+
+  switch (action.type) {
+    case ADD_MOVIES:
+      return {
+        ...state,
+        list: action.movies,
+      };
+    case ADD_TO_FAVOURITES:
+      return {
+        ...state,
+        favourites: [action.movie, ...state.favourites],
+      };
+    case REMOVE_FROM_FAVOURITES:
+      const filteredArray = state.favourites.filter(
+        (movie) => movie.Title !== action.movie.Title
+      );
+      return {
+        ...state,
+        favourites: filteredArray,
+      };
+    case SET_SHOW_FAVOURITES:
+      return {
+        ...state,
+        showFavourites: action.val,
+      };
+
+    default:
+      return state;
+  }
 }
-export default function movies (state = initialMoviesState, action) {
-    // if(action.type === ADD_MOVIES) {
-    //     return {
-    //         ...state,
-    //         list: action.movies
-    //     }
-    // }
-    // return state;
-
-    switch(action.type){
-        case ADD_MOVIES:
-            return {
-                ...state,
-                list: action.movies
-            }
-        case ADD_TO_FAVOURITES: 
-            return {
-                ...state,
-                favourites: [action.movie, ...state.favourites]
-            }
-        case REMOVE_FROM_FAVOURITES: 
-
-            const filteredArray = state.favourites.filter(
-                movie => movie.Title !== action.movie.Title
-            );
-            return {
-                ...state,
-                favourites: filteredArray
-            };
-        case SET_SHOW_FAVOURITES:
-            return {
-                ...state,
-                showFavourites: action.val
-            }
-
-        default:
-            return state
-    }
+const initialSearchState = {
+  result: {},
+};
+export function search(state = initialSearchState, action) {
+  return state;
 }
 
+// const initialRootState = {
+//   movies: initialMoviesState,
+//   search: initialSearchState,
+// };
+// export default function rootReducer(state = initialRootState, action) {
+//   return {
+//     movies: movies(state.movies, action),
+//     search: search(state.search, action),
+//   };
+// }
+
+export default combineReducers({
+  movies: movies,
+  search: search,
+});
