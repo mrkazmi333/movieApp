49a385999d7bf83aff1504b06827288df1a928b6
mrkcancode <motasimkazmi333@gmail.com>
2020-08-23 20:36:40 +0530

    Updates

---
+++ precache-manifest.42ce13f5bbf78eb642f794e70dd88225.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "924da7dccbc5b13f11dbc96991ae3740",
+    "url": "/movieApp/index.html"
+  },
+  {
+    "revision": "e7977e07f2b305732316",
+    "url": "/movieApp/static/css/main.57fbc47d.chunk.css"
+  },
+  {
+    "revision": "16db53900118e73f4916",
+    "url": "/movieApp/static/js/2.dccb859c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movieApp/static/js/2.dccb859c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7977e07f2b305732316",
+    "url": "/movieApp/static/js/main.422077dc.chunk.js"
+  },
+  {
+    "revision": "c5c7366ff97e095278ea",
+    "url": "/movieApp/static/js/runtime-main.7be71e74.js"
+  }
+]);
